6f19b2f31b492a656fb2271c8895b3ca7512a89b
dpushkraj07 <45451225+dpushkraj07@users.noreply.github.com>
2020-06-07 22:31:19 +0530

    Add files via upload
    https://www.hackerrank.com/challenges/js10-inheritance/problem

---
+++ Day5Inheritance.js
@@ -0,0 +1,18 @@
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */Rectangle.prototype.area = function() {
+    return ( this.w * this.h );
+}
+
+class Square extends Rectangle {
+    constructor(s) {
+        super(s, s);
+    }
+}
